9403123410faa369d98b57096459fa42335bdd76
Arthur <arthurpcristovao@gmail.com>
2022-08-03 00:24:20 -0300

    Update and rename URI 1064 Positivos e media.c to PositivosEMedia.c

--- PositivosEMedia.c
+++ PositivosEMedia.c
@@ -1,25 +1,3 @@
-/*#include <stdio.h>
-
-int main() {
-    float num, positivos = 0, media;
-    int i, countPositivos = 0;
-  
-    for ( i = 1; i < 7; i++ ){
-        printf("Digite um numero: ");
-        scanf("%f", &num);
-
-        if (num > 0) {
-            positivos += num;
-            countPositivos += 1;
-        }
-    }
-    media = positivos / countPositivos;
-
-    printf("%d valores positivos\n", countPositivos);
-    printf("%.2f", media);
-    return 0;
-}*/
-
 #include <stdio.h>
 
 int main(){
